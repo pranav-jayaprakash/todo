dde7eac7284a698c1b48c25853b2f6b7a8a957d3
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-07-26 18:12:46 +0530

    Update 2

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import {configureStore} from '@reduxjs/toolkit' 
+import {configureStore} from '@reduxjs/toolkit' ;
+import UserReducer from './Components/UserReducer';
 
 
 const store = configureStore({
   reducer: {
-    // user:
+    users:UserReducer
   }
 })
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
