4889407d13da78c5e2ddb790d4522e3905934d7e
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-07-26 09:59:58 +0530

    update

---
+++ src/Components/UserReducer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {createSlice} from '@reduxjs/toolkit'
+
+
+
+function UserReducer() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default UserReducer
--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import {configureStore} from '@reduxjs/toolkit' 
+
+
+const store = configureStore({
+  reducer: {
+    // user:
+  }
+})
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={}>
     <App />
+    </Provider>
+    
   </React.StrictMode>
 );
 
